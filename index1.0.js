9a13f444d59aa09b643ad0a2e681ea0a5560e00f
shy2850 <shy2850@163.com>
2015-06-02 20:41:50 +0800

    perfect 1.0 with NewsPlayer

--- index1.0.js
+++ index1.0.js
@@ -1,5 +1,5 @@
 (function(){
-    // Script for Player 1.7, by tvst from varal.org
+    // Script for NiftyPlayer 1.7, by tvst from varal.org
     // Released under the MIT License: http://www.opensource.org/licenses/mit-license.php
 
     var FlashHelper = {
@@ -14,36 +14,28 @@
         {
         if (navigator.appName.indexOf ("Microsoft") !=-1) return window[movieName];
           else return document[movieName];
-        },
-        callSWF: function (swf, func, args) {
-            try{
-                return swf[func].apply(swf, args);
-            }catch(e){
-                console.log(func);
-            }
         }
     };
 
-    function Player(name){
+    function niftyplayer(name){
         this.obj = FlashHelper.getMovie(name);
 
         if (!FlashHelper.movieIsLoaded(this.obj)) return;
 
-        var call = FlashHelper.callSWF;
-
         this.play = function () {
-            call( this.obj, "play2", [] );
+            this.obj.TCallLabel('/','play');
         };
         this.pause = function () {
-            call( this.obj, "pause", [] );
+            this.obj.TCallLabel('/','pause');
         };
         this.load = function (url) {
-            call( this.obj, "resetSound", [url,5000] );
+            this.obj.SetVariable('currentSong', url);
+            this.obj.TCallLabel('/','load');
         };
         return this;
     }
 
-    /**player end**/
+    /**niftyplayer end**/
 
 
 	if( "[object HTMLAudioElement]" === ({}).toString.call( window.Audio && new window.Audio ) ){
@@ -67,66 +59,66 @@
 
         //支持情况下，拿到相对的flash路径
 		var baseUrl = document.scripts[document.scripts.length-1].src.replace("index.js",""),
-            swfUrl = baseUrl + "swf/NewsPlayer.swf";
+            swfUrl = baseUrl + "swf/niftyplayer.swf";
 
 		//flash承载体模拟Audio
 		window.Audio = function (id){
 			var div = document.createElement("div");
-            id = id || "NewsPlayer";
-            
-            
-            
+            id = id || "niftyPlayer";
             div.style.position = "absolute";
 			div.style.left = "-20000em"; // 悄悄躲起来不让看见
 			div.innerHTML = 
                 '<object id="'+id+'" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="170" height="100" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0">'+
-                '    <param name="movie" value="'+swfUrl+'?dix=Audio.'+id+'.">'+
+                '    <param name="movie" value="'+swfUrl+'?file=&amp;as=0">'+
                 '    <param name="quality" value="high">'+
                 '    <param name="bgcolor" value="#FFFFFF">'+
-                '    <embed src="'+swfUrl+'?dix=Audio.'+id+'." quality=high bgcolor=#FFFFFF width="170" height="100" name="'+id+'" type="application/x-shockwave-flash" swLiveConnect="true" pluginspage="http://www.macromedia.com/go/getflashplayer">'+
+                '    <embed src="'+swfUrl+'?file=&amp;as=0" quality=high bgcolor=#FFFFFF width="170" height="100" name="'+id+'" type="application/x-shockwave-flash" swLiveConnect="true" pluginspage="http://www.macromedia.com/go/getflashplayer">'+
                 '    </embed>'+
                 '</object>';
 
             // load方法代替src设置
             div.load = function(path){
-                new Player(id).load(path);
+                new niftyplayer(id).load(path);
             };
             // 此方法无用, 原flash不支持
             div.setPercent = function(per){
-                FlashHelper.callSWF( new Player(id).obj, "setProgress", [per]);
+                //TODO
             };
             // play 方法模拟
             div.play = function(){
-                new Player(id).play();
+                new niftyplayer(id).play();
             };
             // pause 方法模拟
             div.pause = function(){
-                new Player(id).pause();
+                new niftyplayer(id).pause();
             }
             // 初始化空方法, 省得调用判断
             div.ontimeupdate = function(){};
             div.onload = function(){};
             div.onended = function(){};
-
-            window.Audio[id] = {
-                setProgress: function(args){
-                    var spl = (args+"").split(",");
-                    div.currentTime = Number(spl[0]) || 0;
-                    div.duration = Number(spl[1]) || 0;
-                    div.ontimeupdate();
-                },
-                playEnd: function(){
-                    div.onended();
-                }
-            };
+            div.duration = 100;     //flash拿不到MP3时长, 姑且用百分比 
             
             var withEnd = false, loaded = false;    // 模拟onended事件需要判断是不是到达结束位置
-            var inter = setInterval(function(){
-                var player = new Player(id), obj = player.obj;
+            setInterval(function(){
+                var player = new niftyplayer(id), obj = player.obj;
                 if( obj && player.load ){
                     if( !loaded ){  // flash加载完成
                         loaded = true;
                         div.onload();
+                    }
+                    try{
+                        // 156.85是flash的内部进度条总长度, pointer._x 是游标位置, 所以currentTime 只是一个表示百分比的数值
+                        div.currentTime = obj.GetVariable("pointer._x") * div.duration / 156.85; 
+                    }catch(e){}
+
+                    if( div.currentTime === div.duration){
+                        if(!withEnd){   // 首次到达结束, 触发
+                            withEnd = true;
+                            div.onended();
+                        }
+                    }else{      // 否则 结束位标识 置false
+                        withEnd = false;
+                        div.ontimeupdate();
                     }
                 }
             },200);
@@ -134,6 +126,6 @@
             return div;
         };
 
-        window.Audio.flash = true; // 标记一下, 使用时候可根据此判断 是否支持设置进度 以及获取MP3时长
+        window.Audio.flash = true;  // 标记一下, 使用时候可根据此判断 是否支持设置进度 以及获取MP3时长
 	}
 })();
